defbccf8fad4e28801210fe8c8dca583d6215a30
Misha Mykhalyuk <m.mikhalyuk@viseven.com>
2017-05-25 21:33:22 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const rootElement = document.getElementById('app')
+
+const appModel = {
+    tasks: [
+        {
+            name: "Read twitter",
+            isDone: false
+        },
+        {
+            name: "Commit changes",
+            isDone: true
+        }
+    ],
+    taskName: '',
+    addTask(event, model){
+        model.tasks.push({
+            name: model.taskName,
+            isDone: false
+        })
+
+        model.taskName = ''
+    },
+    removeTask(event, model){
+        model.tasks.splice(model.index, 1)
+    },
+    removeAllDone(event, model){
+        model.tasks
+            .filter(task => task.isDone)
+            .forEach(task => {
+                const taskIndex = model.tasks.findIndex(item => item === task)
+
+                model.tasks.splice(taskIndex, 1)
+            })
+    }
+}
+
+rivets.bind(rootElement, appModel)
